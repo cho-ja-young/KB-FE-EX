623f3559397c1fb1fe072567501ef81f5351a8a4
cho-ja-young <26006@naver.com>
2025-03-25 17:43:50 +0900

    Add : 20250325 study code add

---
+++ Study/02_Vue/02_Vue/ch07/test-vue-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>App 컴포넌트</h2>
+    <hr />
+    <ul>
+      <CheckboxItem></CheckboxItem>
+      <CheckboxItem></CheckboxItem>
+      <CheckboxItem></CheckboxItem>
+      <CheckboxItem></CheckboxItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'NCT', checked: true },
+        { id: 2, name: 'aespa', checked: true },
+        { id: 3, name: 'KISS OF LIFE', checked: true },
+        { id: 4, name: 'BTS', checked: true },
+      ],
+    };
+  },
+};
+</script>
---
+++ Study/02_Vue/02_Vue/ch07/test-vue-app/src/App2.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>관심있는 K-POP 가수 ?</h2>
+    <hr />
+    <ul>
+      <CheckboxItem
+        v-for="idol in idols"
+        :key="idol.id"
+        :name="idol.name"
+        :checked="idol.checked"
+      ></CheckboxItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem2.vue';
+
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'NCT', checked: true },
+        { id: 2, name: 'aespa', checked: true },
+        { id: 3, name: 'KISS OF LIFE', checked: true },
+        { id: 4, name: 'BTS', checked: true },
+      ],
+    };
+  },
+};
+</script>
