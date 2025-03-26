c0ccbc3162e6cbb40ce3efd81df1ef86fd25fd17
cho-ja-young <26006@naver.com>
2025-03-26 16:06:03 +0900

    Add : 20250326 basic code - study v-model-test add

---
+++ Study/02_Vue/02_Vue/ch08/v-model-test/src/components/ChildClickCount.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>자식의 자식 클릭 Count : {{ cnt }}</h1>
+    <!-- App.vue 에서 가져온 Count -->
+    <button @click="increment">클릭 시 1 증가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildClickCount',
+
+  // provide 로 제공된 정보 삽입하기
+  inject: ['cnt', 'increment'],
+};
+</script>
---
+++ Study/02_Vue/02_Vue/ch08/v-model-test/src/components/ClickCount.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>자식 클릭 Count : {{ cnt }}</h1>
+    <!-- App.vue 에서 가져온 Count -->
+    <!-- <button @click="increment">클릭 시 1 증가</button> -->
+  </div>
+  <ChildClickCount></ChildClickCount>
+</template>
+
+<script>
+import ChildClickCount from './ChildClickCount.vue';
+
+export default {
+  name: 'ClickCount',
+  components: { ChildClickCount },
+
+  // provide 로 제공된 정보 삽입하기
+  //   inject: ['cnt', 'increment'],
+  inject: ['cnt'],
+};
+</script>
