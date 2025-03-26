9e8821d501adb9d1474d65202aeab68de81ff1f7
cho-ja-young <26006@naver.com>
2025-03-26 15:58:51 +0900

    Add : 20250326 basic code - study slot-test add

---
+++ Study/02_Vue/02_Vue/ch08/slot-test/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NoSlotTest></NoSlotTest>
+    <SlotTest></SlotTest>
+  </div>
+</template>
+
+<script>
+import NoSlotTest from './components/NoSlotTest.vue';
+import SlotTest from './components/SlotTest.vue';
+
+export default {
+  name: 'App',
+  components: { NoSlotTest, SlotTest },
+};
+</script>
---
+++ Study/02_Vue/02_Vue/ch08/slot-test/src/components/ScopedSlotTest.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3>당신이 경험한 프론트엔드 기술은? (네번째:slot기본)</h3>
+
+    <CheckBoxList>
+      <template> </template>
+      <template> </template>
+    </CheckBoxList>
+  </div>
+</template>
+
+<script>
+import CheckBox2 from './CheckBox2.vue';
+
+export default {
+  name: 'SlotTest',
+  components: { CheckBox2 },
+  data() {
+    return {
+      items: [
+        { id: 'V', checked: true, label: 'Vue' },
+        { id: 'A', checked: false, label: 'Angular' },
+        { id: 'R', checked: false, label: 'React' },
+        { id: 'S', checked: false, label: 'Svelte' },
+      ],
+    };
+  },
+  methods: {
+    CheckBoxChanged(e) {
+      let item = this.items.find((item) => item.id === e.id);
+      item.checked = e.checked;
+    },
+  },
+};
+</script>
