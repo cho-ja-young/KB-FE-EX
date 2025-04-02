3f990272309439525fd3398b268a196901771419
cho-ja-young <26006@naver.com>
2025-04-02 17:49:59 +0900

    Add : 20250328 심화 문제 레이아웃 코드 작성

--- Practice/02_Vue/02_Vue/20250328/hard/fake-store/src/App.vue
+++ Practice/02_Vue/02_Vue/20250328/hard/fake-store/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="container">
-    <Header></Header>
-    <NavBar></NavBar>
-    <router-view class="mt-5 ms-3"></router-view>
-    <Footer></Footer>
-  </div>
+  <DefaultLayout>
+    <template v-slot:header><Header></Header></template>
+    <template v-slot:navbar><NavBar></NavBar></template>
+    <template v-slot:default
+      ><router-view class="mt-5 ms-3"></router-view
+    ></template>
+    <template v-slot:footer><Footer></Footer></template>
+  </DefaultLayout>
 </template>
 
 <script setup>
+import { RouterView } from 'vue-router';
+import DefaultLayout from './components/layouts/DefaultLayout.vue';
+
 import Header from './components/layouts/Header.vue';
 import NavBar from './components/layouts/NavBar.vue';
 import Footer from './components/layouts/Footer.vue';
---
+++ Practice/02_Vue/02_Vue/20250328/hard/fake-store/src/components/layouts/NavBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Nav Bar</h1>
+</template>
+
+<script setup></script>
