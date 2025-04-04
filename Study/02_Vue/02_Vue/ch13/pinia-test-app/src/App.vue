3ccbb4e0b582442200f9d535f4edb8d5c4d2debf
cho-ja-young <26006@naver.com>
2025-04-04 15:10:25 +0900

    Add : 20250404 study Ex pinia

---
+++ Study/02_Vue/02_Vue/ch13/pinia-test-app/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter.js';
+
+const store = useCounterStore();
+const { count, increment } = store;
+// const count = store.count;
+
+increment();
+
+console.log(store);
+</script>
+
+<template>
+  {{ store.count }}/{{ store.doubleCount }} <br />
+  {{ count }}
+</template>
